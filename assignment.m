c0bef76833fd5ec5f4c1ecaf855b653ec306b896
WJChoong <61902435+WJChoong@users.noreply.github.com>
2023-08-22 23:55:46 +0800

    update GUI

--- assignment.m
+++ assignment.m
@@ -8,6 +8,12 @@ function fakeCurrencyDetectionGui()
     pathText = uicontrol('Style', 'text', 'Position', [50, 200, 300, 30], 'HorizontalAlignment', 'left');
     resultText = uicontrol('Style', 'text', 'Position', [50, 150, 300, 30], 'HorizontalAlignment', 'center');
 
+     % Create radio buttons
+    radioMethod1 = uicontrol('Style', 'radio', 'String', '10', 'Position', [50, 160, 100, 20], 'Tag', 'method1');
+    radioMethod2 = uicontrol('Style', 'radio', 'String', '20', 'Position', [150, 160, 100, 20], 'Tag', 'method2');
+    radioMethod3 = uicontrol('Style', 'radio', 'String', '50', 'Position', [250, 160, 100, 20], 'Tag', 'method3');
+    radioMethod4 = uicontrol('Style', 'radio', 'String', '100', 'Position', [350, 160, 100, 20], 'Tag', 'method4');
+
     % Initialize variables
     selectedImagePath = '';
 
@@ -26,8 +32,9 @@ function fakeCurrencyDetectionGui()
         if isempty(selectedImagePath)
             msgbox('Please select an image first.', 'Error', 'error');
         else
+            method = getSelectedMethod(); % Get the selected method
             % Perform fake currency detection using the 6 functions
-            result = performFakeCurrencyDetection(selectedImagePath);
+            result = performFakeCurrencyDetection(selectedImagePath, method);
             
             if result
                 resultMessage = 'The image is not detected as fake currency.';
@@ -39,48 +46,67 @@ function fakeCurrencyDetectionGui()
         end
     end
 
-    % Function to perform fake currency detection using 6 features
-    function result = performFakeCurrencyDetection(imagePath)
-        result = true; % Initialize result as true (image is initially considered fake)
-    
-        if ~checkFeature1(imagePath) || ~checkFeature2(imagePath) || ...
-           ~checkFeature3(imagePath) || ~checkFeature4(imagePath) || ...
-           ~checkFeature5(imagePath) || ~checkFeature6(imagePath)
-            result = false; % If any feature indicates the image is NOT fake currency, set result to false
+    % Get the selected detection method
+    function method = getSelectedMethod()
+        if get(radioMethod1, 'Value')
+            method = '1';
+        elseif get(radioMethod2, 'Value')
+            method = '2';
+        elseif get(radioMethod3, 'Value')
+            method = '3';
+        elseif get(radioMethod4, 'Value')
+            method = '4';
+        else
+            method = '0'; 
         end
     end
 
+    % Function to perform fake currency detection using 6 features
+    function result = performFakeCurrencyDetection(imagePath, method)
+        result = true;
+    
+        if ~checkFeature1(imagePath, method) || ~checkFeature2(imagePath, method) || ...
+           ~checkFeature3(imagePath, method) || ~checkFeature4(imagePath, method) || ...
+           ~checkFeature5(imagePath, method) || ~checkFeature6(imagePath, method)
+            result = false; 
+        end
+    end
 
     % Placeholder functions for fake currency detection features
-    function result = checkFeature1(imagePath)
-        % Implement feature 1 detection logic
-        % Return true if the feature is detected as indicative of fake currency
-        result = true; % Placeholder logic
+    function result = checkFeature1(imagePath, method)
+        selectedMethod = getSelectedMethod();
+        fprintf('Selected Method: %s\n', selectedMethod);
+        result = true;
     end
 
-    function result = checkFeature2(imagePath)
-        % Implement feature 2 detection logic
-        result = true; % Placeholder logic
+    function result = checkFeature2(imagePath, method)
+        selectedMethod = getSelectedMethod();
+        fprintf('Selected Method: %s\n', selectedMethod);
+        result = true;
     end
     
-    function result = checkFeature3(imagePath)
-        % Implement feature 3 detection logic
-        result = true; % Placeholder logic
+    function result = checkFeature3(imagePath, method)
+        selectedMethod = getSelectedMethod();
+        fprintf('Selected Method: %s\n', selectedMethod);
+        result = true;
     end
 
-    function result = checkFeature4(imagePath)
-        % Implement feature 4 detection logic
-        result = true; % Placeholder logic
+    function result = checkFeature4(imagePath, method)
+        selectedMethod = getSelectedMethod();
+        fprintf('Selected Method: %s\n', selectedMethod);
+        result = true;
     end
 
-    function result = checkFeature5(imagePath)
-        % Implement feature 5 detection logic
-        result = true; % Placeholder logic
+    function result = checkFeature5(imagePath, method)
+        selectedMethod = getSelectedMethod();
+        fprintf('Selected Method: %s\n', selectedMethod);
+        result = true;
     end
 
-    function result = checkFeature6(imagePath)
-        % Implement feature 6 detection logic
-        result = true; % Placeholder logic
+    function result = checkFeature6(imagePath, method)
+        selectedMethod = getSelectedMethod();
+        fprintf('Selected Method: %s\n', selectedMethod);
+        result = true;
     end
 
 end
