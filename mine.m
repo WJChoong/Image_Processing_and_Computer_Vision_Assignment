85c7781f2c8ec3055ab2641408cd54ef4328899b
Felicia Ng <feliciayin197@gmail.com>
2023-08-25 15:37:37 +0800

    hi

--- mine.m
+++ mine.m
@@ -4,11 +4,11 @@ currentFolderPath = pwd;
 disp(['Current folder path: ' currentFolderPath]);
 
 % Load the real image
-imagePath ='C:\Users\Felicia\OneDrive\Desktop\100.png';
+imagePath ='C:\Users\Felicia\OneDrive\Desktop\Currency\Real\10.png';
 currencyImage = imread(imagePath);
 
 % Input Image
-imagePathInput ='C:\Users\Felicia\OneDrive\Desktop\F4.png';
+imagePathInput ='C:\Users\Felicia\OneDrive\Desktop\Currency\10\F2.png';
 currencyImageInput = imread(imagePathInput);
 
 % Determine the maximum dimensions
@@ -19,27 +19,61 @@ maxHeight = max(size(currencyImage, 1), size(currencyImageInput, 1));
 currencyImage = imresize(currencyImage, [maxHeight, maxWidth]);
 currencyImageInput = imresize(currencyImageInput, [maxHeight, maxWidth]);
 
-% Define the dimensions of the crop region
-cropWidth = 670;   % Specify the width of the crop region
-cropHeight = 100;  % Specify the height of the crop region
-cropTopOffset = 20; % Specify the offset from the top to adjust cropping
-cropRightOffset = 650; % Specify the offset from the right to adjust cropping
+
+option = "10"; 
+
+
+if option == "100"
+    % Define the dimensions of the crop region
+    cropWidth = 1000;    % Specify the width of the crop region
+    cropHeight = 100;    % Specify the height of the crop region
+    cropTopOffset = 20;  % Specify the offset from the top to adjust cropping
+    cropLeftOffset = 150; % Specify the offset from the left to adjust cropping
+    cropRightOffset = 690;% Specify the offset from the right to adjust cropping
+
+elseif option == "50"
+    % Define the dimensions of the crop region
+    cropWidth = maxWidth / 2;    % Specify the width of the crop region
+    cropHeight = maxHeight / 4;  % Specify the height of the crop region
+    cropTopOffset = 15;  % Specify the offset from the top to adjust cropping
+    cropLeftOffset = 150; % Specify the offset from the left to adjust cropping
+    cropRightOffset = 400;% Specify the offset from the right to adjust cropping
+
+elseif option == "20"
+    % Define the dimensions of the crop region
+    cropWidth = maxWidth / 2;    % Specify the width of the crop region
+    cropHeight = maxHeight / 4;    % Specify the height of the crop region
+    cropTopOffset = 20;  % Specify the offset from the top to adjust cropping
+    cropLeftOffset = 300; % Specify the offset from the left to adjust cropping
+    cropRightOffset = 200;% Specify the offset from the right to adjust cropping
+
+elseif option == "10"
+    % Define the dimensions of the crop region
+    cropWidth = maxWidth / 2.5;    % Specify the width of the crop region
+    cropHeight = maxHeight / 4;    % Specify the height of the crop region
+    cropTopOffset = 20;  % Specify the offset from the top to adjust cropping
+    cropLeftOffset = 530; % Specify the offset from the left to adjust cropping
+    cropRightOffset = 400;% Specify the offset from the right to adjust cropping
+
+end
+
 
 % Calculate the coordinates for cropping (top middle)
-cropTopLeftX = floor((size(currencyImage, 2) - cropWidth) / 2);
-cropTopLeftY = cropTopOffset + 1; % Adjusted to include the top offset
-cropBottomRightX = size(currencyImage, 2) - cropRightOffset;
+cropTopLeftX = cropLeftOffset + 1; % Adjusted to include the left offset
+cropTopLeftY = cropTopOffset + 1;  % Adjusted to include the top offset
+cropBottomRightX = cropTopLeftX + cropWidth - 1; % Adjusted to include the left offset and width
 cropBottomRightY = cropTopOffset + cropHeight;
 
-% Calculate the coordinates for cropping (top middle)
-cropTopLeftXInput = floor((size(currencyImageInput, 2) - cropWidth) / 2);
-cropTopLeftYInput = cropTopOffset + 1; % Adjusted to include the top offset
-cropBottomRightXInput = size(currencyImageInput, 2) - cropRightOffset;
+% Calculate the coordinates for cropping (top middle) for input image
+cropTopLeftXInput = cropLeftOffset + 1; % Adjusted to include the left offset
+cropTopLeftYInput = cropTopOffset + 1;  % Adjusted to include the top offset
+cropBottomRightXInput = cropTopLeftXInput + cropWidth - 1; % Adjusted to include the left offset and width
 cropBottomRightYInput = cropTopOffset + cropHeight;
 
 % Perform cropping
 croppedImage = currencyImage(cropTopLeftY:cropBottomRightY, cropTopLeftX:cropBottomRightX, :);
 croppedImageInput = currencyImageInput(cropTopLeftYInput:cropBottomRightYInput, cropTopLeftXInput:cropBottomRightXInput, :);
+
 
 % Create a figure
 figure;
@@ -74,6 +108,33 @@ disp('Extracted Text from Input Image:');
 disp(recognizedTextInput);
 
 
+% Convert recognized texts to uppercase for consistent comparison
+recognizedTextCroppedUpper = upper(recognizedTextCropped);
+recognizedTextInputUpper = upper(recognizedTextInput);
+
+% Define the target text to check for
+targetText = 'BANK NEGARA MALAYSIA';
+
+% Check if the target text is present in the recognized texts
+isTargetPresentCropped = contains(recognizedTextCroppedUpper, targetText);
+isTargetPresentInput = contains(recognizedTextInputUpper, targetText);
+
+% Determine if the currency is real or fake based on target text presence
+if isTargetPresentCropped
+    disp('Real Currency: "BANK NEGARA MALAYSIA" detected in cropped image.');
+else
+    disp('Fake Currency: "BANK NEGARA MALAYSIA" not detected in cropped image.');
+end
+
+if isTargetPresentInput
+    disp('Real Currency: "BANK NEGARA MALAYSIA" detected in input image.');
+else
+    disp('Fake Currency: "BANK NEGARA MALAYSIA" not detected in input image.');
+end
+
+
+
+%%
 % Convert recognized texts to lowercase
 recognizedTextCropped = lower(recognizedTextCropped);
 recognizedTextInput = lower(recognizedTextInput);
